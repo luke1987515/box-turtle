699baed74f6060d58534e9468624bb935204c2ba
陳業淵 <luke1987515@hotmail.com>
2017-11-28 09:43:02 +0800

    Change create diskpart_script.conf in run time

--- OS_rebooter_script/check_hdd_number.bat
+++ OS_rebooter_script/check_hdd_number.bat
@@ -10,10 +10,18 @@ echo off
 :: 5. 檢查結果 
 
 cls
+echo.
 echo #####################################################
 echo ### 檢查 HDD 數量的工具 V1.0.0
 echo #####################################################
+
+:: 判斷 diskpart_script.conf 是否存在
+IF NOT EXIST diskpart_script.conf (
 echo.
+echo ### 不存在  diskpart_script.conf        檔案 ###
+echo list disk > diskpart_script.conf
+echo ###   建立  diskpart_script.conf        檔案 ###
+)
 
 :: 判斷 gold_hdd_list.txt 是否存在
 IF EXIST gold_hdd_list.txt (
@@ -25,9 +33,9 @@ echo.
 echo #####################################################
 echo !!!! 已存在 gold_hdd_list.txt !!!!
 echo.
-echo ### 如果這不是你要的 gold_hdd_list.txt 檔案 ###
-echo ### 按下  Y/y   刪除 gold_hdd_list.txt 檔案 ###
-echo ###     5 sec 後保留繼續...                 ### 
+echo ###  如果這不是你要的 gold_hdd_list.txt 檔案 ###
+echo ###  按下  Y/y   刪除 gold_hdd_list.txt 檔案 ###
+echo ###        5 sec 保留繼續...                 ### 
 
 choice /C YN /T 5 /D N /M "Would you like to clear 'gold_hdd_list.txt' ??" 
 if errorlevel 2 goto normalc
@@ -35,11 +43,11 @@ if errorlevel 1 goto clearc
 
 :clearc
 del gold_hdd_list.txt
-echo ###  刪除舊 gold_hdd_list.txt 檔案 ###
+echo ###  刪除舊           gold_hdd_list.txt 檔案 ###
 echo.
 echo !!!! 請確認 HDD 數量正確 !!!!
 echo #####################################################
-diskpart /s diskpart_script.txt
+diskpart /s diskpart_script.conf
 echo.
 echo #####################################################
 echo !!!! 請確認 HDD 數量正確 !!!!
@@ -47,13 +55,13 @@ diskpart /s diskpart_script.conf > gold_hdd_list.txt
 goto end_choice1
 
 :normalc
-echo ###  保留舊 gold_hdd_list.txt 檔案 ###
+echo ###  保留舊           gold_hdd_list.txt 檔案 ###
 goto end_choice1
 
 ) ELSE (
 echo.
-echo ###  不存在 gold_hdd_list.txt 檔案 ###
-echo ###  建立   gold_hdd_list.txt 檔案 ###
+echo ###  不存在           gold_hdd_list.txt 檔案 ###
+echo ###    建立           gold_hdd_list.txt 檔案 ###
 echo.
 echo !!!! 請確認 HDD 數量正確 !!!!
 echo #####################################################
@@ -66,27 +74,27 @@ diskpart /s diskpart_script.conf > gold_hdd_list.txt
 
 :end_choice1
 echo.
-echo ###  完成 gold_hdd_list.txt 設置 ###
+echo ###  完成             gold_hdd_list.txt 設置 ###
 timeout /t 3 /nobreak
 
 :: count 執行次數
 IF NOT EXIST count.txt (
 echo.
-echo ### 不存在 count.txt   檔案 ###
+echo ###  不存在                   count.txt 檔案 ###
 set /a "number=0"
 echo 0 > count.txt
-echo ### 建立   count.txt   檔案 ###
+echo ###  建立                     count.txt 檔案 ###
 
 ) ELSE (
 echo.
 echo !!!! 已存在 count.txt !!!!
 echo Count number：
 echo #####################################################
-type count.txt
+FOR /F "eol=" %%i in (count.txt) do (echo ###  Count number： %%i%)
 echo #####################################################
-echo ### 如果這不是你要的 count 次數 ###
-echo ### 按下  Y/y   歸零 count 次數 ###
-echo ###     5 sec 後繼續計數...     ### 
+echo ###  如果這不是你要的             count 次數 ###
+echo ###  按下  Y/y   歸零             count 次數 ###
+echo ###        5 sec 後繼續計數...               ### 
 
 choice /C YN /T 5 /D N /M "Would you like to clear 'count.txt' ??" 
 if errorlevel 2 goto normalc_count
@@ -94,16 +102,16 @@ if errorlevel 1 goto clearc_count
 
 :clearc_count
 del count.txt
-echo ### 刪除舊 count.txt    檔案 ###
-echo ### 歸零   count number 次數 ###
+echo ###  刪除舊                   count.txt 檔案 ###
+echo ###  歸零                  count number 次數 ###
 set /a "number=0"
 echo 0 > count.txt
 goto end_choice2
 
 :normalc_count
-echo ### 保留舊 count.txt    檔案 ###
+echo ###  保留舊                   count.txt 檔案 ###
 type count.txt
-echo ### 使得   count number 加 1 ###
+echo ###  使得                  count number 加 1 ###
 FOR /F "eol= " %%i in (count.txt) do (set /a "number=%%i%+1")
 echo %number%
 echo %number% > count.txt
@@ -112,7 +120,7 @@ goto end_choice2
 
 :end_choice2
 echo.
-echo ### 完成   count.txt   設置 ###
+echo ###  完成                     count.txt 設置 ###
 echo.
 FOR /F "eol=" %%i in (count.txt) do (echo 現在是第 %%i% 次)
 timeout /t 3 /nobreak
@@ -123,14 +131,19 @@ set hh=%time:~0,2%
 if "%time:~0,1%"==" " set hh=0%hh:~1,1%
 set dt=%date:~0,4%-%date:~5,2%-%date:~8,2%_%hh%_%time:~3,2%_%time:~6,2%
 
-:: 如果 Count number 歸零，清空 result.log 檔案  
-if "%number%"=="0" (echo %dt% > result.log)
+:: 如果 Count number 歸零，清空 result.log 紀錄、刪除 hdd_list_*.txt 
+if "%number%"=="0" (
+echo %dt% > result.log
+if EXIST hdd_list_*.txt ( del /F hdd_list_*.txt )
+)
 
-echo ### 開始與原始資料(gold_hdd_list.txt)比對 ###
+echo ###  開始與原始資料(gold_hdd_list.txt) 比對 ###
 diskpart /s diskpart_script.conf
 diskpart /s diskpart_script.conf > hdd_list_%number%.txt
 
 echo.
-echo ### 比對結果 ###
+echo ###  比對結果                               ###
+echo #####################################################
 FC gold_hdd_list.txt hdd_list_%number%.txt
 FC gold_hdd_list.txt hdd_list_%number%.txt >> result.log
+echo #####################################################
