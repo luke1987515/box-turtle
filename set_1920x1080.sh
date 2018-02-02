f7c0069f53c34d393eb02ef9c3057ae659b23eee
root <root@localhost.localdomain>
2018-02-02 11:44:52 +0800

    Put file in /etc/profile.d

--- set_1920x1080.sh
+++ set_1920x1080.sh
@@ -1,7 +1,16 @@
-xrandr
-##gtf -x 1920 1080 60
+#!/bin/bash
+# CentOS 7.4
+# Put "set_1920x1080.sh" in "/etc/profile.d"
+
+#xrandr
+##gtf 1920 1080 60 -x
+#xrandr --newmode "1920x1080"  172.80  1920 2040 2248 2576  1080 1081 1084 1118  -HSync +Vsync
+#xrandr --addmode VGA-1 "1920x1080"
+#xrandr --output VGA-1 --mode "1920x1080"
+#xrandr
+
 #cvt -r 1920 1080 60
-xrandr --newmode "1920x1080R"  138.50  1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
-xrandr --addmode VGA-1 "1920x1080R"
-xrandr --output VGA-1 --mode "1920x1080R"
-xrandr
+#xrandr --newmode "1920x1080"  138.50  1920 1968 2000 2080  1080 1083 1088 1111 +hsync -vsync
+#xrandr --addmode VGA-1 "1920x1080"
+#xrandr --output VGA-1 --mode "1920x1080"
+#xrandr
