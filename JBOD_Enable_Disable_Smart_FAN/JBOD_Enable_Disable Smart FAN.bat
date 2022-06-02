069f8c0bc10267ba95d6d3c4f5a4377676d9b0cf
陳業淵 <luke1987515@hotmail.com>
2022-06-02 16:39:58 +0800

    Add get 1:7:1 bit info

--- JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
+++ JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
@@ -12,6 +12,11 @@ echo Enable JBOD SamrtFAN
 for /F %%i IN ('sg_scan -s ^| find "AIC"') DO set expaddress=%%i
 set exp=%expaddress:~0,17%
 sg_ses --descriptor=CoolingElement00 --clear=1:7:1 %exp%
+
+ehco sg_ses --descriptor=CoolingElement00 --get=1:7:1 %exp%
+sg_ses --descriptor=CoolingElement00 --get=1:7:1 %exp%
+echo GET 0 is Enable Auto FAN - Seccess
+echo GET 1 is Disable Auto FAN - Seccess
 pause
 exit
 
