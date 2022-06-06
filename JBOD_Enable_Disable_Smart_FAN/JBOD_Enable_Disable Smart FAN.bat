e79e297a385c549767cc800c9de3d06b0559e90c
陳業淵 <luke1987515@hotmail.com>
2022-06-06 02:35:47 +0000

    Add able to set muti Enclosure

--- JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
+++ JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
@@ -1,32 +1,48 @@
 @echo off
 ::sg_scan -s to Show the device for AIC Expander Controller 
 
+setlocal EnableDelayedExpansion
+
 set /p Set=Input (1:Enable 2:Disable):
 
-if "%Set%"=="1" goto A
-if "%Set%"=="2" goto B
+::Enable JBOD SamrtFAN, clear 1:7:1
+if "%Set%"=="1" set "operator=clear"
+::Disable JBOD SamrtFAN, set 1:7:1
+if "%Set%"=="2" set "operator=set"
+:: Wrong Input
+if not "%Set%"=="1" if not "%Set%"=="2" goto :END 
 
-::Enable JBOD SamrtFAN
-:A
+echo ====================
 echo Enable JBOD SamrtFAN
-for /F %%i IN ('sg_scan -s ^| find "AIC"') DO set expaddress=%%i
-set exp=%expaddress:~0,17%
-sg_ses --descriptor=CoolingElement00 --clear=1:7:1 %exp%
+echo ====================
+echo.
+for /F %%i in ('sg_scan -s ^| find "AIC"') do (
+  echo ====================
+  echo Enclosure : "%%i"
+  echo --------------------
+  sg_ses --descriptor=CoolingElement00 --%operator%=1:7:1 %%i
+  echo sg_ses --descriptor=CoolingElement00 --%operator%=1:7:1 %%i
+  echo sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i
+  :: sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i
+  sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i > info_A.tmp
+  echo --------------------
+  for /f %%a in (info_A.tmp) do (
+  set "var=%%a"
+    if "%Set%"=="1" if "!var!"=="0" echo GET 0 : Enable JBOD SamrtFAN - Seccess
+    if "%Set%"=="1" if "!var!"=="1" echo GET 1 : Enable JBOD SamrtFAN - FAILED
+	if "%Set%"=="2" if "!var!"=="0" echo GET 0 : Disable JBOD SamrtFAN - FAILED
+    if "%Set%"=="2" if "!var!"=="1" echo GET 1 : Disable JBOD SamrtFAN - Seccess
+  )
+  echo ====================
+  echo.
+)
 
-ehco sg_ses --descriptor=CoolingElement00 --get=1:7:1 %exp%
-sg_ses --descriptor=CoolingElement00 --get=1:7:1 %exp%
-echo GET 0 is Enable Auto FAN - Seccess
-echo GET 1 is Disable Auto FAN - Seccess
+if exist *.tmp del /Q *.tmp
+
 pause
 exit
 
-::Disable JBOD SamrtFAN
-:B
-echo Disable JBOD SamrtFAN
-for /F %%i IN ('sg_scan -s ^| find "AIC"') DO set expaddress=%%i
-set exp=%expaddress:~0,17%
-sg_ses --descriptor=CoolingElement00 --set=1:7:1 %exp%
-pause
-exit 
+:END
+echo NOT 1 or 2 , Wrong Input 
 
 pause
