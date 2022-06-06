08c2f1eea4fd4974308260f46bcf6df871153d58
陳業淵 <luke1987515@hotmail.com>
2022-06-06 02:58:46 +0000

    Fix "show Enable or Disable title info ""

--- JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
+++ JBOD_Enable_Disable_Smart_FAN/JBOD_Enable_Disable Smart FAN.bat
@@ -3,34 +3,36 @@
 
 setlocal EnableDelayedExpansion
 
+cls
+
 set /p Set=Input (1:Enable 2:Disable):
 
 ::Enable JBOD SamrtFAN, clear 1:7:1
-if "%Set%"=="1" set "operator=clear"
+if "%Set%"=="1" set "operation=Enable"&set "operator=clear"
 ::Disable JBOD SamrtFAN, set 1:7:1
-if "%Set%"=="2" set "operator=set"
+if "%Set%"=="2" set "operation=Disable"&set "operator=set"
 :: Wrong Input
 if not "%Set%"=="1" if not "%Set%"=="2" goto :END 
 
 echo ====================
-echo Enable JBOD SamrtFAN
+echo %operation% JBOD SamrtFAN
 echo ====================
 echo.
 for /F %%i in ('sg_scan -s ^| find "AIC"') do (
   echo ====================
   echo Enclosure : "%%i"
   echo --------------------
-  sg_ses --descriptor=CoolingElement00 --%operator%=1:7:1 %%i
-  echo sg_ses --descriptor=CoolingElement00 --%operator%=1:7:1 %%i
-  echo sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i
-  :: sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i
-  sg_ses --descriptor=CoolingElement00 --get=1:7:1 %%i > info_A.tmp
+  sg_ses --descriptor=SysCoolingElement00 --%operator%=1:7:1 %%i
+  echo sg_ses --descriptor=SysCoolingElement00 --%operator%=1:7:1 %%i
+  echo sg_ses --descriptor=SysCoolingElement00 --get=1:7:1 %%i
+  :: sg_ses --descriptor=SysCoolingElement00 --get=1:7:1 %%i
+  sg_ses --descriptor=SysCoolingElement00 --get=1:7:1 %%i > info_A.tmp
   echo --------------------
   for /f %%a in (info_A.tmp) do (
   set "var=%%a"
     if "%Set%"=="1" if "!var!"=="0" echo GET 0 : Enable JBOD SamrtFAN - Seccess
     if "%Set%"=="1" if "!var!"=="1" echo GET 1 : Enable JBOD SamrtFAN - FAILED
-	if "%Set%"=="2" if "!var!"=="0" echo GET 0 : Disable JBOD SamrtFAN - FAILED
+    if "%Set%"=="2" if "!var!"=="0" echo GET 0 : Disable JBOD SamrtFAN - FAILED
     if "%Set%"=="2" if "!var!"=="1" echo GET 1 : Disable JBOD SamrtFAN - Seccess
   )
   echo ====================
