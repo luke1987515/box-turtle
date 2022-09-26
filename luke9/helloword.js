23ff2c9a1d239f6d1819a7a4199982943145a7d6
陳業淵 <luke1987515@hotmail.com>
2022-09-26 12:30:27 +0800

    luke9

---
+++ luke9/helloword.js
@@ -0,0 +1,23 @@
+   
+      var memory = new  WebAssembly.Memory({initial:1});
+
+      function  consoleLogString(offset, length) {
+        var  bytes = new  Uint8Array(memory.buffer, offset, length);
+        var  string = new  TextDecoder('utf8').decode(bytes);
+        console.log(string);
+      };
+
+      var  importObject = {
+        console: {
+          log:  consoleLogString
+        },
+        js : {
+          mem:  memory
+        }
+      };
+     
+      WebAssembly.instantiateStreaming(fetch('helloworld.wasm'), importObject)
+      .then(obj  => {
+        obj.instance.exports.helloWorld();
+      });
+     
