b2f20bc343340d372ccf042375fd7ef1a700ec29
陳業淵 <luke1987515@hotmail.com>
2023-05-04 16:44:27 +0800

    Update LED Red & Blue status

--- JBOD_LED_Behavior/J4078-01_LED_red_1sec_off_1sec.bat
+++ JBOD_LED_Behavior/J4078-01_LED_red_1sec_off_1sec.bat
@@ -43,21 +43,22 @@ for %%a in (%list%) do (
 goto loop
 
 :: Array Device Slot control element
-:: set list=1:7:1 1:6:1 1:5:1 1:4:1 1:3:1 1:2:1 1:1:1 1:0:1 2:7:1 2:6:1 2:4:1 2:3:1 2:2:1 2:1:1 3:5:1 3:4:1
+:: set list=1:7:1 1:6:1 1:5:1 1:4:1 1:3:1 1:2:1 1:1:1 1:0:1 2:7:1 2:6:1 2:4:1 2:3:1 2:2:1 2:1:1 3:5:1 3:4:1 0:6:1
 
-:: set list=1:7:1 (RQST OK)
-:: set list=1:6:1 (RQST RSVD DEVICE)
-:: set list=1:5:1 (RQST HOT SPARE)
-:: set list=1:4:1 (RQST CONS CHECK)
-:: set list=1:3:1 (RQST IN CRIT ARRAY)
-:: set list=1:2:1 (RQST IN FAILED ARRAY)
-:: set list=1:1:1 (RQST REBULD/REMAP)
-:: set list=1:0:1 (RQST R/R ABORT)
-:: set list=2:7:1 (RQST ACTIVE)
-:: set list=2:6:1 (DO NOT REMOVE)
-:: set list=2:4:1 (RQST MISSING)
-:: set list=2:3:1 (RQST INSERT)
-:: set list=2:2:1 (RQST REMOVE)
-:: set list=2:1:1 (RQST IDENT)
-:: set list=3:5:1 (RQST FAULT)
-:: set list=3:4:1 (DEVICE OFF)
+:: set list=1:7:1 (RQST OK) --------------| Activity | OFF 
+:: set list=1:6:1 (RQST RSVD DEVICE) -----| Activity | OFF 
+:: set list=1:5:1 (RQST HOT SPARE) -------| Activity | OFF 
+:: set list=1:4:1 (RQST CONS CHECK) ------| Activity | Fast Blink
+:: set list=1:3:1 (RQST IN CRIT ARRAY)----| Activity | Slow Blink
+:: set list=1:2:1 (RQST IN FAILED ARRAY)--| Activity | Slow Blink
+:: set list=1:1:1 (RQST REBULD/REMAP) ----| Activity | Fast Blink
+:: set list=1:0:1 (RQST R/R ABORT) -------| Activity | Slow Blink
+:: set list=2:7:1 (RQST ACTIVE) ----------| Activity | OFF
+:: set list=2:6:1 (DO NOT REMOVE) --------| Activity | OFF
+:: set list=2:4:1 (RQST MISSING) ---------| ON | ON
+:: set list=2:3:1 (RQST INSERT) ----------| Activity | Slow Blink
+:: set list=2:2:1 (RQST REMOVE) ----------| Activity | Slow Blink
+:: set list=2:1:1 (RQST IDENT) -----------| Slow Blink | Slow Blink
+:: set list=3:5:1 (RQST FAULT) -----------| ON | ON
+:: set list=3:4:1 (DEVICE OFF) -----------| OFF | OFF 
+:: set list=0:6:1 (DEVICE OFF) -----------| Activity | Slow Blink
