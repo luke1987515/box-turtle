5da0171cfe4d96b2ede37420f620c8b9e1f5b763
陳業淵 <luke1987515@hotmail.com>
2023-10-04 10:33:54 +0800

    Update J4078-01_LED_red_1sec_off_1sec.bat
    add PRDFAIL LED status

--- JBOD_LED_Behavior/J4078-01_LED_red_1sec_off_1sec.bat
+++ JBOD_LED_Behavior/J4078-01_LED_red_1sec_off_1sec.bat
@@ -6,6 +6,7 @@
 @echo off
 
 set enclosure_ID=SCSI3:6,16,0
+:: set enclosure_ID=SCSI3:6,16,0
 set list=3:5:1
 
 :loop
@@ -17,27 +18,35 @@ for %%a in (%list%) do (
     for /l %%x in (1, 1, 9) do (
         echo sg_ses.exe --descriptor=Disk00%%x --set=%%a %enclosure_ID%
         sg_ses.exe --descriptor=Disk00%%x --set=%%a %enclosure_ID%
+		ping 127.0.0.1 -n 3 > nul
+		sg_ses.exe %enclosure_ID% --descriptor=Disk0%%x
     )
     for /l %%x in (10, 1, 78) do (
         echo sg_ses.exe --descriptor=Disk0%%x --set=%%a %enclosure_ID%
-        sg_ses.exe --descriptor=Disk0%%x --set=%%a %enclosure_ID% 
+        sg_ses.exe --descriptor=Disk0%%x --set=%%a %enclosure_ID%
+		ping 127.0.0.1 -n 3 > nul
+		sg_ses.exe %enclosure_ID% --descriptor=Disk0%%x
     )
 :: Sleep 1 sec
-    ping 127.0.0.1 -n 1 > nul
+    ping 127.0.0.1 -n 3 > nul
 
 :: Set LED status off
 ::
     for /l %%x in (1, 1, 9) do (
-         echo sg_ses.exe --descriptor=Disk00%%x --clear=%%a %enclosure_ID%
-         sg_ses.exe --descriptor=Disk00%%x --clear=%%a %enclosure_ID%
+        echo sg_ses.exe --descriptor=Disk00%%x --clear=%%a %enclosure_ID%
+        sg_ses.exe --descriptor=Disk00%%x --clear=%%a %enclosure_ID%
+		ping 127.0.0.1 -n 3 > nul
+		sg_ses.exe %enclosure_ID% --descriptor=Disk0%%x
     )
     for /l %%x in (10, 1, 78) do (
         echo sg_ses.exe --descriptor=Disk0%%x --clear=%%a %enclosure_ID%
         sg_ses.exe --descriptor=Disk0%%x --clear=%%a %enclosure_ID%
+		ping 127.0.0.1 -n 3 > nul
+		sg_ses.exe %enclosure_ID% --descriptor=Disk0%%x
     )
 
 :: Sleep 1 sec
-    ping 127.0.0.1 -n 1 > nul
+    ping 127.0.0.1 -n 3 > nul
 )
 
 goto loop
@@ -45,20 +54,20 @@ goto loop
 :: Array Device Slot control element
 :: set list=1:7:1 1:6:1 1:5:1 1:4:1 1:3:1 1:2:1 1:1:1 1:0:1 2:7:1 2:6:1 2:4:1 2:3:1 2:2:1 2:1:1 3:5:1 3:4:1 0:6:1
 
-:: set list=1:7:1 (RQST OK) --------------| Activity | OFF 
-:: set list=1:6:1 (RQST RSVD DEVICE) -----| Activity | OFF 
-:: set list=1:5:1 (RQST HOT SPARE) -------| Activity | OFF 
-:: set list=1:4:1 (RQST CONS CHECK) ------| Activity | Fast Blink
-:: set list=1:3:1 (RQST IN CRIT ARRAY)----| Activity | Slow Blink
-:: set list=1:2:1 (RQST IN FAILED ARRAY)--| Activity | Slow Blink
-:: set list=1:1:1 (RQST REBULD/REMAP) ----| Activity | Fast Blink
-:: set list=1:0:1 (RQST R/R ABORT) -------| Activity | Slow Blink
-:: set list=2:7:1 (RQST ACTIVE) ----------| Activity | OFF
-:: set list=2:6:1 (DO NOT REMOVE) --------| Activity | OFF
-:: set list=2:4:1 (RQST MISSING) ---------| ON | ON
-:: set list=2:3:1 (RQST INSERT) ----------| Activity | Slow Blink
-:: set list=2:2:1 (RQST REMOVE) ----------| Activity | Slow Blink
-:: set list=2:1:1 (RQST IDENT) -----------| Slow Blink | Slow Blink
-:: set list=3:5:1 (RQST FAULT) -----------| ON | ON
-:: set list=3:4:1 (DEVICE OFF) -----------| OFF | OFF 
-:: set list=0:6:1 (DEVICE OFF) -----------| Activity | Slow Blink
+:: set list=1:7:1 (RQST OK)               Blue LED: Activity      Red LED: OFF
+:: set list=1:6:1 (RQST RSVD DEVICE)      Blue LED: Activity      Red LED: OFF
+:: set list=1:5:1 (RQST HOT SPARE)        Blue LED: Activity      Red LED: OFF
+:: set list=1:4:1 (RQST CONS CHECK)       Blue LED: Activity      Red LED: Fast Blink
+:: set list=1:3:1 (RQST IN CRIT ARRAY)    Blue LED: Activity      Red LED: Slow Blink
+:: set list=1:2:1 (RQST IN FAILED ARRAY)  Blue LED: Activity      Red LED: Slow Blink
+:: set list=1:1:1 (RQST REBULD/REMAP)     Blue LED: Activity      Red LED: Fast Blink
+:: set list=1:0:1 (RQST R/R ABORT)        Blue LED: Activity      Red LED: Slow Blink
+:: set list=2:7:1 (RQST ACTIVE)           Blue LED: Activity      Red LED: OFF
+:: set list=2:6:1 (DO NOT REMOVE)         Blue LED: Activity      Red LED: OFF
+:: set list=2:4:1 (RQST MISSING)          Blue LED: ON            Red LED: ON
+:: set list=2:3:1 (RQST INSERT)           Blue LED: Activity      Red LED: Slow Blink
+:: set list=2:2:1 (RQST REMOVE)           Blue LED: Activity      Red LED: Slow Blink
+:: set list=2:1:1 (RQST IDENT)            Blue LED: Slow Blink    Red LED: OFF
+:: set list=3:5:1 (RQST FAULT)            Blue LED: ON            Red LED: ON 
+:: set list=3:4:1 (DEVICE OFF)            Blue LED: OFF           Red LED: OFF
+:: set list=0:6:1 (PRDFAIL)               Blue LED: Activity      Red LED: Slow Blink
