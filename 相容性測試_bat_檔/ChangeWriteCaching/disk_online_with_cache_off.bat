d0ed0ee954d5d69b2c406a7028b88dad95ab08c6
陳業淵 <luke1987515@hotmail.com>
2024-03-15 15:50:20 +0800

    Update disk_online_with_cache_off.bat
    Support sort disk index, more easy read.

--- 相容性測試_bat_檔/ChangeWriteCaching/disk_online_with_cache_off.bat
+++ 相容性測試_bat_檔/ChangeWriteCaching/disk_online_with_cache_off.bat
@@ -14,6 +14,8 @@
 
 @echo off
 
+setlocal EnableDelayedExpansion
+
 echo ################
 echo Disk Online Tool
 echo ################
@@ -48,7 +50,36 @@ echo Total %DiskNum% Disk Online!
 echo #######################
 echo.
 
-for /f "skip=1" %%a in ('wmic diskdrive get Index') do ( 
+echo ################
+echo Get diskdrive get Index
+echo ################
+::ic diskdrive get   %%i
+wmic diskdrive get Index  > diskdrive.tmp
+type diskdrive.tmp > diskdrive.txt
+
+:: Sort diskdrive get Index 
+:: I don't know why but IT WORK!!
+:: ==============================================
+for /f "skip=1 tokens=1" %%i in (diskdrive.txt) do (
+	set /A a=1000+%%i
+    set "line[!a:~1!]=%%i
+)
+
+
+::Show array elements
+::sort diskdrive.txt
+for /F "tokens=2 delims==" %%a in ('set line[') do echo %%a >> disksort_index.tmp
+type disksort_index.tmp
+
+:: ===============================================
+
+echo.
+echo #######################
+echo Total %DiskNum% Disk cache off!
+echo #######################
+echo.
+
+for /f "skip=1" %%a in (disksort_index.tmp) do ( 
  echo PHYSICALDRIVE%%a
  dskcache.exe -w PHYSICALDRIVE%%a
 )
@@ -58,5 +89,15 @@ echo #######################
 echo Total %DiskNum% Disk cache off!
 echo #######################
 echo.
+
+for /f "skip=1" %%a in (disksort_index.tmp) do ( 
+ echo PHYSICALDRIVE%%a
+ dskcache.exe PHYSICALDRIVE%%a | findstr /C:"Write Cache is enable"
+ 
+ )
+
+if exist diskdrive.tmp del /Q diskdrive.tmp
+if exist diskdrive.txt del /Q diskdrive.txt
+if exist disksort_index.tmp del /Q disksort_index.tmp
 
 PAUSE
